61d2e3cfb0c78aab32195b4c901f0a369a0cb63d
sanjoy <sanjoykundu187@gamil.com>
2023-07-22 15:53:31 +0600

    all image added

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,10 @@
 // import { ref, } from 'vue';
 import { ref, reactive } from 'vue';
 
-const images = ref([  //REACTIVE IS NOT WORKING JUST CHECK REF AND CATCH SINGLE VALUE
-  { id: 1, url: 'https://www.creativeitinstitute.com/images/course/course_1663052056.jpg' },
-  { id: 2, url: 'https://www.creativeitinstitute.com/images/course/course_1673929467.jpg' },
-  { id: 3, url: 'https://www.creativeitinstitute.com/images/course/course_1663052296.jpg' },
-  { id: 4, url: 'https://www.creativeitinstitute.com/images/course/course_1663052131.jpg' },
+const images = ref([  //catch single card value
+  { id: 1, url: "https://images.unsplash.com/photo-1682685797769-481b48222adf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60" },
+  { id: 2, url: "https://images.unsplash.com/photo-1682695794816-7b9da18ed470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60" },
+  { id: 3, url: "https://images.unsplash.com/photo-1682685797661-9e0c87f59c60?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE2fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=1024&q=60" }
 ]);
 
 const currentImage = ref(1);
@@ -25,7 +24,7 @@ const prevImage = () => {
     <h1 class="mb-10">This is my Carousel </h1>
     <div class="carousel">
         <div v-for="image in images" :key="image.id" v-show="image.id == currentImage ">
-          <img :src="image.url" alt="Carousel Image" class="w-full h-auto rounded-lg" />
+          <img :src="image.url" alt="Carousel Image" class="w-50% h-auto rounded-lg" />
         </div>
     </div>
     <div class="carousel-nav mt-5">
